c7fbe789ee088e1aaf0ba01f7fe3e4d0331fa3f5
Da0111zk <da.azocard@duocuc.cl>
2026-05-19 01:18:27 -0400

    Delete ms-kardex/src/main/java/com/example/ms_stock directory

--- ms-kardex/src/main/java/com/example/ms_stock/Repository/StockRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.ms_stock.Repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.ms_stock.modelo.Stock;
-public interface StockRepository extends JpaRepository<Stock, Long> {
-
-}
